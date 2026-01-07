9c035c8620e73ba7e32123dcdb791b99148d5a96
Stephen Lewis <code@stephenlewis.me>
2026-01-07 23:14:12 +0000

    Add 1Password Flatpak

--- flatpak.sh
+++ flatpak.sh
@@ -33,3 +33,6 @@ flatpaks=(
 for f in "${flatpaks[@]}"; do
     flatpak install --noninteractive --or-update flathub $f
 done
+
+# 1Password is a special snowflake
+flatpak install --noninteractive https://downloads.1password.com/linux/flatpak/1Password.flatpakref
